042727e7825eb647a64fcfd258f608b08550d094
renansouzzzz <renansouza.oliv@outlook.com>
2024-07-10 18:00:38 -0300

    interface domain

---
+++ MacorattiCleanArchtecture.Domain/Entities/Company.cs
@@ -0,0 +1,19 @@
+﻿namespace MacorattiCleanArchtecture.Domain.Entities
+{
+    public sealed class Company : EntityAbstract
+    {
+        public string Name { get; private set; }
+
+        public string CNPJ { get; private set; }
+
+        public string Address { get; private set; }
+
+
+        public Company(string name, string cnpj, string address)
+        {
+            Name = name;
+            CNPJ = cnpj;
+            Address = address;
+        }
+    }
+}
---
+++ MacorattiCleanArchtecture.Domain/Interfaces/ICompanyService.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MacorattiCleanArchtecture.Domain.Interfaces
+{
+    public interface ICompanyService
+    {
+        public void Select();
+
+        public void Insert();
+
+        public void Update();
+
+        public void Delete();
+    }
+}
