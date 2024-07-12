05707dc1d6cf070012393f75a68e1b83f1319566
renansouzzzz <renansouza.oliv@outlook.com>
2024-07-11 21:07:14 -0300

    company service

---
+++ MacorattiCleanArchtecture.Application/Service/CompanyService.cs
@@ -0,0 +1,27 @@
+﻿using MacorattiCleanArchtecture.Domain.Interfaces;
+
+namespace MacorattiCleanArchtecture.Application.Service
+{
+    public class CompanyService : ICompanyService
+    {
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
