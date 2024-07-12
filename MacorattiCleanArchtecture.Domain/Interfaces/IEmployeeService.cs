c59b61ed6afbc84ba5e4be9c20ba6bdc66a92b8a
renansouzzzz <renansouza.oliv@outlook.com>
2024-07-11 21:01:34 -0300

    interfaces

---
+++ MacorattiCleanArchtecture.Domain/Interfaces/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MacorattiCleanArchtecture.Domain.Interfaces
+{
+    public interface IEmployeeService
+    {
+        public void Select();
+
+        public void Insert();
+
+        public void Update();
+
+        public void Delete();
+    }
+}
